7b3b92e064e778c6a7155f1e2094497efa00f7ec
Thai Truong <thaicwru@gmail.com>
2021-07-13 17:06:04 -0400

    Add model view set

--- gathered/gathered_api/views.py
+++ gathered/gathered_api/views.py
@@ -1,23 +1,33 @@
 from rest_framework import generics
 from gathered.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import BasePermission, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, SAFE_METHODS
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework import viewsets
+from rest_framework import filters
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+
 
 class PostUserWritePermission(BasePermission):
-    message = 'Editing posts is restricted to the author only'
+    message = 'Editing posts is restricted to the author only.'
+
     def has_object_permission(self, request, view, obj):
+
         if request.method in SAFE_METHODS:
             return True
+
         return obj.author == request.user
 
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-    queryset = Post.postobjects.all()
+class PostList(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
 
+    def get_object(self, queryset=None, **kwargs):
+        item = self.kwargs.get('pk')
+        return get_object_or_404(Post, slug=item)
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
-    permission_classes = [PostUserWritePermission]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    # Define Custom Queryset
+    def get_queryset(self):
+        return Post.objects.all()
+
